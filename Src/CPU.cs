8c249648b9cd9dd3f0dbae7a749f02343538c00b
Callum Edwards <126828563+Calx03@users.noreply.github.com>
2023-12-01 21:18:09 +0000

    Initial opcodes.

--- Src/CPU.cs
+++ Src/CPU.cs
@@ -17,6 +17,26 @@
         public byte DelayTimer { get; set; }
         public byte SoundTimer { get; set; }
 
+        private readonly byte[] fonts =
+        {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+
         public CPU()
         {
             Memory = new byte[4096];
@@ -33,7 +53,30 @@
 
         public void Cycle()
         {
+            Opcode = (ushort)(Memory[PC] << 8 | Memory[PC + 1]);
 
+            switch (Opcode & 0xF000)
+            {
+                case 0x1000:
+                    Ins_1xNNN(Opcode);
+                    break;
+
+                case 0x6000:
+                    Ins_6xKK(Opcode);
+                    break;
+            }
+        }
+
+        private void Ins_1xNNN(ushort opcode)
+        {
+            PC = (ushort)(opcode & 0x0FFF);
+        }
+
+        private void Ins_6xKK(ushort opcode)
+        {
+            var register = (opcode & 0x0F00) >> 8;
+            V[register] = (byte)(opcode & 0x00FF);
+            PC += 2;
         }
     }
 }
